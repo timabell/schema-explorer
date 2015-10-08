d3f4566062c0ecc537398a9c99846d5d491ff5e9
Tim Abell <tim@timwise.co.uk>
2015-10-08 20:01:31 +0100

    hello world web server
    https://golang.org/doc/articles/wiki/

--- sdv.go
+++ sdv.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"net"
+	"fmt"
 	"net/http"
-	"net/http/fcgi"
 )
 
-type FastCGIServer struct{}
-
-func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	resp.Write([]byte("<h1>bonjour!</h1>\n<p>Hello soapie</p>"))
+func handler(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resp, "<h1>bonjour!</h1>\n<p>Hello soapie</p>")
 }
 
 func main() {
-	listener, _ := net.Listen("tcp", "127.0.0.1:80")
-	srv := new (FastCGIServer)
-	fcgi.Serve(listener, srv)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
