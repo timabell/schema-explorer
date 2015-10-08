b6080c5bb99179c76b54997c3b80818638a25b66
Tim Abell <tim@timwise.co.uk>
2015-10-08 18:21:58 +0100

    fast cgi server example
    don't think this actually what I wanted
    
    http://mwholt.blogspot.co.uk/2013/05/writing-go-golang-web-app-with-nginx.html

--- sdv.go
+++ sdv.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"net/http"
+	"net/http/fcgi"
+)
+
+type FastCGIServer struct{}
+
+func (s FastCGIServer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("<h1>bonjour!</h1>\n<p>Hello soapie</p>"))
+}
 
 func main() {
-	fmt.Println("Hello soapie")
+	listener, _ := net.Listen("tcp", "127.0.0.1:80")
+	srv := new (FastCGIServer)
+	fcgi.Serve(listener, srv)
 }
