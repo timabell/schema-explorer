6ec51d8f286a9422babb5b2ba534463cf2b10fa4
Tim Abell <tim@timwise.co.uk>
2015-10-10 21:30:33 +0100

    Show db path on page

--- sdv.go
+++ sdv.go
@@ -6,12 +6,15 @@ import (
 	"os"
 )
 
+var db string
+
 func handler(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(resp, "<h1>bonjour!</h1>\n<p>Hello soapie</p>")
+	fmt.Fprintf(resp, "<p>Connected to %s</p>", db)
 }
 
 func main() {
-	db := os.Args[1]
+	db = os.Args[1]
 	fmt.Printf("Connecting to db: %s\n", db)
 	http.HandleFunc("/", handler)
 	fmt.Println("Listening on http://localhost:8080/")
