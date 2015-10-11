5e18afd5b0c1ebcc394b5d234b12f7db301c2797
Tim Abell <tim@timwise.co.uk>
2015-10-11 01:02:29 +0100

    sqlite connection opened

--- sdv.go
+++ sdv.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var db string
 
 func handler(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(resp, "<h1>bonjour!</h1>\n<p>Hello soapie</p>")
+	dbc, err :=sql.Open("sqlite3", db)
+	if (err != nil) {
+		fmt.Println("connection error", err)
+	}
+	defer dbc.Close()
 	fmt.Fprintf(resp, "<p>Connected to %s</p>", db)
 }
 
