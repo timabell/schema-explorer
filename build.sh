50732796451eac908929230d9c9c0824dc7901d9
Tim Abell <tim@timwise.co.uk>
2015-10-16 22:10:38 +0100

    Todo for win x-compile

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 go build -o bin/sdv-linux-x64 sdv.go
+
+# todo - cross compile for windows
+# - sdv.go:17:2: C source files not allowed when not using cgo: sqlite3-binding.c
+# GOOS=windows go build -o bin/sdv-windows-x64 sdv.go
