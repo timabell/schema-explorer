178c7462ee771437584bf489faf632c9f1b2981d
Tim Abell <tim@timwise.co.uk>
2017-06-08 22:36:53 +0100

    Test sql server connection - get server version
    https://mathaywardhill.com/2017/04/27/get-started-with-golang-and-sql-server-in-visual-studio-code/

--- sdv.go
+++ sdv.go
@@ -17,6 +17,8 @@ import (
 	"os"
 	"strconv"
 	"sql-data-viewer/sdv"
+	_ "github.com/denisenkom/go-mssqldb"
+	"database/sql"
 )
 
 var db string
@@ -24,6 +26,24 @@ var db string
 // var pageTemplate template.Template
 
 func main() {
+	conn, errdb := sql.Open("mssql", "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT")
+	defer conn.Close()
+	if errdb != nil {
+		panic(errdb)
+	}
+	rows, errqry := conn.Query("select @@version;")
+	if errqry != nil {
+		panic(errqry)
+	}
+	var sqlversion string
+	rows.Next()
+	errrslt := rows.Scan(&sqlversion)
+	if errrslt != nil {
+		panic(errrslt)
+	}
+	log.Print(sqlversion)
+	return
+
 	if len(os.Args) <= 1 {
 		log.Fatal("missing argument: path to sqlite database file")
 	}
