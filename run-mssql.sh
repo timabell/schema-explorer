f418e714c3c83db5cd09c56fb4f123b310b5d1fe
Tim Abell <tim@timwise.co.uk>
2017-07-01 21:57:35 +0100

    Example run sh files

---
+++ run-mssql.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./sdv-linux-x64 mssql "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT" 8080 localhost
---
+++ run-sqlite.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./sdv-linux-x64 sqlite ~/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite 8081 localhost
