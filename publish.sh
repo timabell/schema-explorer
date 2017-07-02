9175277fbaf1fef3d8be74d87bb2e82df5cfc2ed
Tim Abell <tim@timwise.co.uk>
2017-07-02 10:00:56 +0100

    Publish as zip instead of tgz
    More windows friendly.
    
    I think linux folk will cope, and windows folk are my first target
    market. Test user #1 (Joel) didn't have a linux machine handy to connect
    to mssql unsurprisingly.

--- publish.sh
+++ publish.sh
@@ -3,5 +3,9 @@ mkdir -p package/sdv/
 ./build.sh
 ./build-win.sh
 cp -r README.md scripts/* bin/* package/sdv/
-tar -czvf sdv.tar.gz -C package sdv
-mv -v sdv.tar.gz ~/Dropbox/share/sdv/
+
+cd package
+zip -r sdv.zip sdv
+mv -v sdv.zip ~/Dropbox/share/sdv/
+# tar -czvf sdv.tar.gz -C package sdv
+# mv -v sdv.tar.gz ~/Dropbox/share/sdv/
