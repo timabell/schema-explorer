1c69e12f75f05de8c26805b183ef873a10ef2abc
Tim Abell <tim@timwise.co.uk>
2017-08-10 23:40:58 +0100

    Docker rm - force deletion
    Multiple tags caused a failure

--- docker-rm-all.sh
+++ docker-rm-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # https://stackoverflow.com/a/30311197/10245
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+docker rm -f $(docker ps -a -q)
+docker rmi -f  $(docker images -q)
