70ebc8eddacec55a225fd459960ffd0631c5a487
Tim Abell <tim@timwise.co.uk>
2017-08-12 10:25:50 +0100

    Publish raw binary to dropbox for docker image

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -v
 ./package.sh
-cd package
-mv -v sdv.zip ~/Dropbox/share/sdv/
+
+# for manual downloads from https://blog.timwise.co.uk/sdv/sdv-download/
+mv -v package/sdv.zip ~/Dropbox/share/sdv/
+
+# for docker image https://github.com/timabell/sdv-docker/blob/master/Dockerfile
+cp -v bin/linux/sdv-linux-x64 ~/Dropbox/share/sdv/
