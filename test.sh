b2f21a7a8c467be89651c8ef467629024161d638
Tim Abell <tim@timwise.co.uk>
2017-08-20 00:31:59 +0100

    Verbose test for mssql

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 go test ./... -driver sqlite -db ~/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite -test.v
 
-go test ./... -driver mssql -db "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT"
+go test ./... -driver mssql -db "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT" -test.v
