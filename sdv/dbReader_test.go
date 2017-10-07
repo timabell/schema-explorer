8805ee369b57c56772eee2f59f1ac63ec317b64a
Tim Abell <tim@timwise.co.uk>
2017-10-07 11:54:57 +0100

    Test - test col name read
    Sqlite pass
    Mssql fail - not implemented yet

--- sdv/dbReader_test.go
+++ sdv/dbReader_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"bitbucket.org/timabell/sql-data-viewer/schema"
 	_ "github.com/mattn/go-sqlite3"
 	_ "github.com/simnalamburt/go-mssqldb"
 )
@@ -48,5 +49,23 @@ func Test_GetTables(t *testing.T) {
 	expectedName := "foo"
 	if table.Name != expectedName {
 		t.Error(fmt.Sprintf("Expected table '%s' found '%s'", expectedName, table.Name))
+	}
+}
+
+func Test_GetColumns(t *testing.T) {
+	reader := getDbReader(testDbDriver, testDb)
+	table := schema.Table{Name: "foo"}
+	columns, err := reader.GetColumns(table)
+	if err != nil {
+		t.Error(err)
+	}
+	expectedCount := 2
+	if len(columns) != expectedCount {
+		t.Error(fmt.Sprintf("Expected %d columns, found %d", expectedCount, len(columns)))
+	}
+	col0 := columns[0]
+	expectedName := "id"
+	if col0.Name != expectedName {
+		t.Error(fmt.Sprintf("Expected column '%s' found '%s'", expectedName, col0))
 	}
 }
