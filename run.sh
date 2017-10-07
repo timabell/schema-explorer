67f13e7ee4e597c8ece763859aace96bd01536ea
Tim Abell <tim@timwise.co.uk>
2017-10-07 11:40:01 +0100

    Add script for running with test databases
    To assist writing and troubleshooting regression test suite.
    Also a handy quick visual test.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -
+# relative path hack with pwd, otherwise not resolved.
+# create db first with sqlite/setup.sh
+go run sdv.go -driver sqlite -db "`pwd`/sqlite/db/test.db" -port 8088 &
+go run sdv.go -driver mssql -db "server=sdv-regression-test.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=sdv-regression-test" -port 8089 &
