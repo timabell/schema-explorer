cfe531edf3039cdbc807f4c244876a38259260ce
Tim Abell <tim@timwise.co.uk>
2017-10-10 23:43:33 +0100

    Run.sh - ports++
    Avoid clash with syncthing on 8080

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -
 # relative path hack with pwd, otherwise not resolved.
 # create db first with sqlite/setup.sh
-go run sdv.go -driver sqlite -db "`pwd`/sqlite/db/test.db" -port 8080 &
-go run sdv.go -driver sqlite -db "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" -port 8081 &
-go run sdv.go -driver mssql -db "server=sdv-regression-test.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=sdv-regression-test" -port 8082 &
-go run sdv.go -driver mssql -db "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT" -port 8083 &
-go run sdv.go -driver mssql -db "server=sdv-wwi.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=WideWorldImporters" -port 8084 &
+go run sdv.go -driver sqlite -db "`pwd`/sqlite/db/test.db" -port 8081 &
+go run sdv.go -driver sqlite -db "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" -port 8082 &
+go run sdv.go -driver mssql -db "server=sdv-regression-test.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=sdv-regression-test" -port 8083 &
+go run sdv.go -driver mssql -db "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT" -port 8084 &
+go run sdv.go -driver mssql -db "server=sdv-wwi.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=WideWorldImporters" -port 8085 &
 wait
