062e660ca562046b3a0dd99bec785e0229d2a81a
Tim Abell <tim@timwise.co.uk>
2017-11-08 22:05:37 +0000

    Remove bare sdv binary from dropbox
    No longer works standalone now there are external templates.

--- publish.sh
+++ publish.sh
@@ -4,7 +4,4 @@
 # for manual downloads from https://blog.timwise.co.uk/sdv/sdv-download/
 mv -v package/sdv.zip ~/Dropbox/share/sdv/
 
-# for docker image https://github.com/timabell/sdv-docker/blob/master/Dockerfile
-cp -v bin/linux/sdv-linux-x64 ~/Dropbox/share/sdv/
-
 echo "Make sure dropbox is running!"
