93fb5b3aa9c0f01a45d2af713ef2fecc43bc945b
Tim Abell <tim@timwise.co.uk>
2018-01-07 18:59:10 +0000

    run-sqlite - add live flag

--- run-sqlite.sh
+++ run-sqlite.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -
 # relative path hack with pwd, otherwise not resolved.
 # create db first with sqlite/setup.sh
-go run sdv.go -driver sqlite -db "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" -port 8082 &
+go run sdv.go -driver sqlite -db "$HOME/Documents/projects/sql-data-viewer/Chinook_Sqlite_AutoIncrementPKs.sqlite" -port 8082 -live &
 wait
