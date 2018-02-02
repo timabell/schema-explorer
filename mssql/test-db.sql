4026fa23076a56ed14d325d4defc6e00676dff7e
Tim Abell <tim@timwise.co.uk>
2018-02-02 03:31:56 +0000

    Updated mssql test schema to match new tests

--- mssql/test-db.sql
+++ mssql/test-db.sql
@@ -5,6 +5,18 @@ if object_id('DataTypeTest', 'U') is not null
 begin
 	drop table DataTypeTest;
 end
+if object_id('pet', 'U') is not null
+begin
+	drop table pet;
+end
+if object_id('toy', 'U') is not null
+begin
+	drop table toy;
+end
+if object_id('person', 'U') is not null
+begin
+	drop table person;
+end
 go
 create table DataTypeTest (
 	intpk integer primary key,
@@ -51,4 +63,31 @@ insert into DataTypeTest (
 	'b470fa05-2111-46f9-9c97-f103b594c5f0'
 )
 ;
-select * from DataTypeTest;
+--select * from DataTypeTest;
+
+create table person (
+	personId int PRIMARY KEY,
+	personName nvarchar(50),
+--	favouritePetId int references pet(petId)
+);
+
+create table pet (
+	petId int PRIMARY KEY,
+	petName nvarchar(50),
+	ownerId int references person(personId),
+	favouritePersonId int references person(personId)
+);
+
+create table toy (
+	toyId int PRIMARY KEY,
+	toyName nvarchar(50),
+	belongsToId int references pet(petId)
+);
+alter table person add favouritePetId int references pet(petId)
+
+insert into person(personId,personName) values(1,'bob'),(2,'fred');
+insert into pet(petId,petName, ownerId, favouritePersonId)values(5, 'kitty',1,2);
+insert into pet(petId,petName, ownerId, favouritePersonId)values(6, 'fido',2,2);
+insert into toy(toyId, toyName, belongsToId) values(11,'mouse',5);
+insert into toy(toyId, toyName, belongsToId) values(12,'ball',6);
+update person set favouritePetId = 5 where personId = 2;
