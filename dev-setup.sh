d101fe14c79cd9791df52d113c7bdff5f3814fbe
Tim Abell <tim@timwise.co.uk>
2018-03-24 14:57:07 +0000

    Dev setup - dependencies - sqlite3 binary and go get libs

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,6 +2,11 @@
 
 # This is more a reminder of the steps than something intended to be run automatically
 
+# needed to set up the test db
+sudo apt install sqlite3
+
+# ================
+
 # Install https://github.com/moovweb/gvm
 
 sudo apt-get install curl git mercurial make binutils bison gcc build-essential
@@ -15,6 +20,8 @@ export GOROOT_BOOTSTRAP=$GOROOT
 gvm install go1.9.4
 gvm use go1.9.4 --default
 
+# ================
+
 # Manually Download & run https://www.jetbrains.com/go/
 
 # manually set goroot & gopath in goland
@@ -22,3 +29,8 @@ echo $GOROOT
 # /home/tim/.gvm/gos/go1.9.4
 echo $GOPATH
 # /home/tim/.gvm/pkgsets/go1.9.4/global:/home/tim/repo/go
+
+# ================
+
+# run this from project root
+go get
