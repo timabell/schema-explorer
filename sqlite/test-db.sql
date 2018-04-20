2a1dad03b2560bcbacfb374c20306f2494dec993
Tim Abell <tim@timwise.co.uk>
2018-04-20 01:21:28 +0100

    Working auto-fit of diagram size/zoom

--- sqlite/test-db.sql
+++ sqlite/test-db.sql
@@ -156,8 +156,30 @@ insert into toy(toyId, toyName, belongsToId) values(11,'mouse',5);
 insert into toy(toyId, toyName, belongsToId) values(12,'ball',6);
 update person set favouritePetId = 5 where personId = 2;
 
+-- tall, for checking diagram scaling manually
+create table up1 ( upId int PRIMARY KEY);
+create table up2 ( upId int PRIMARY KEY, anotherId int references up1(upId));
+create table up3 ( upId int PRIMARY KEY, anotherId int references up2(upId));
+create table up4 ( upId int PRIMARY KEY, anotherId int references up3(upId));
+create table up5 ( upId int PRIMARY KEY, anotherId int references up4(upId));
+create table up6 ( upId int PRIMARY KEY, anotherId int references up5(upId));
+create table up7 ( upId int PRIMARY KEY, anotherId int references up6(upId));
+create table up8 ( upId int PRIMARY KEY, anotherId int references up7(upId));
+create table up9 ( upId int PRIMARY KEY, anotherId int references up8(upId));
+create table up10 ( upId int PRIMARY KEY, anotherId int references up9(upId));
+create table up11 ( upId int PRIMARY KEY, anotherId int references up10(upId));
+create table up12 ( upId int PRIMARY KEY, anotherId int references up11(upId));
+create table up13 ( upId int PRIMARY KEY, anotherId int references up12(upId));
+create table up14 ( upId int PRIMARY KEY, anotherId int references up13(upId));
+create table up15 ( upId int PRIMARY KEY, anotherId int references up14(upId));
+create table up16 ( upId int PRIMARY KEY, anotherId int references up15(upId));
+create table up17 ( upId int PRIMARY KEY, anotherId int references up16(upId));
+create table up18 ( upId int PRIMARY KEY, anotherId int references up17(upId));
+create table up19 ( upId int PRIMARY KEY, anotherId int references up18(upId));
+create table up20 ( upId int PRIMARY KEY, anotherId int references up19(upId));
+
 -- wide, for checking diagram scaling manually
-create table parent ( parentId int PRIMARY KEY );
+create table parent ( parentId int PRIMARY KEY, grandParentId int references up20(upId));
 create table child01 ( childId int PRIMARY KEY, parentId int references parent(parentId));
 create table child02 ( childId int PRIMARY KEY, parentId int references parent(parentId));
 create table child03 ( childId int PRIMARY KEY, parentId int references parent(parentId));
