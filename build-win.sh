dde03dab97ccd7e8b2d462f27bffb84ce5af5d2d
Tim Abell <tim@timwise.co.uk>
2018-05-04 20:04:36 +0100

    Build-dep for named pipes

--- build-win.sh
+++ build-win.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# prereq: apt-get install gcc-mingw-w64
+
+# prereqs:
+#   apt-get install gcc-mingw-w64
+#   go get -v -d -t gopkg.in/natefinch/npipe.v2
+
 # https://github.com/mattn/go-sqlite3/issues/106#issuecomment-240179249
+
 echo running windows build, this takes a while...
 env CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -o bin/windows/sql-data-viewer.exe
 cp -r templates bin/windows/
