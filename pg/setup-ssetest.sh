c09687481a865ab6781bd00a5511bd474f41efd1
Tim Abell <tim@timwise.co.uk>
2018-05-08 09:43:35 +0100

    Set owner first

--- pg/setup-ssetest.sh
+++ pg/setup-ssetest.sh
@@ -5,7 +5,7 @@ dropdb $db
 dropuser $usr
 createuser $usr
 createdb $db
-psql -d $db < test-db.sql
 psql -c "alter user $usr with password '$usr'";
 psql -c "alter database $db owner to $usr";
+psql -d $db < test-db.sql
 psql -d $db -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $usr;";
