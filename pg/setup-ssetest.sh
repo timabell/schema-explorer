25a4a28b466f403c4a9539bd6d5ea1e731a59117
Tim Abell <tim@timwise.co.uk>
2018-05-09 08:05:09 +0100

    Postgres test foreign keys set up done

--- pg/setup-ssetest.sh
+++ pg/setup-ssetest.sh
@@ -7,5 +7,6 @@ createuser $usr
 createdb $db
 psql -c "alter user $usr with password '$usr'";
 psql -c "alter database $db owner to $usr";
-psql -d $db < test-db.sql
+# use psql -e to echo sql along with errors while debugging
+psql -d $db -e < test-db.sql
 psql -d $db -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $usr;";
--- pg/test-db.sql
+++ pg/test-db.sql
@@ -1,19 +1,9 @@
--- sqlite example db for regression tests
+-- postgres example db for regression tests
 -- schema must match test code's expectations
 
-drop table if exists DataTypeTest;
+create table datatypetest ( intpk integer primary key, colCount int, field_INT INT );
 
-create table DataTypeTest (
-	intpk integer primary key,
-	colCount int,
-	field_INT INT
-);
-
-insert into DataTypeTest(
-	intpk,
-	colCount,
-	-- https://www.sqlite.org/datatype3.html#affinity_name_examples
-	field_INT
+insert into datatypetest( intpk, colCount, field_INT
 )values(
 	10, --intpk
 	3, --colCount
@@ -24,86 +14,28 @@ insert into DataTypeTest(
 	-33 --INT
 );
 
--- select * from DataTypeTest;
-
-drop table if exists pet;
-drop table if exists person;
-drop table if exists toy;
-
 create table toy (
-	toyId int PRIMARY KEY,
-	toyName varchar(50)
--- 	belongsToId int references pet(petId)
+	toyId int PRIMARY KEY, toyName varchar(50),
+	belongsToId int --references pet(petId)
 );
 
 create table person (
-	personId int PRIMARY KEY,
-	personName varchar(50)
--- 	favouritePetId int references pet(petId)
+	personId int PRIMARY KEY, personName varchar(50),
+	favouritePetId int --references pet(petId)
 );
 
 create table pet (
-	petId int PRIMARY KEY,
-	petName varchar(50),
-	ownerId int references person(personId),
+	petId int PRIMARY KEY, petName varchar(50), ownerId int references person(personId),
 	favouritePersonId int references person(personId)
 );
+
+alter table toy add foreign key (belongsToId) REFERENCES pet (petId);
+alter table person add foreign key (favouritePetId) REFERENCES pet (petId);
 
 insert into person(personId,personName) values(1,'bob'),(2,'fred');
 insert into pet(petId,petName, ownerId, favouritePersonId)values(5, 'kitty',1,2);
 insert into pet(petId,petName, ownerId, favouritePersonId)values(6, 'fido',2,2);
--- insert into toy(toyId, toyName, belongsToId) values(11,'mouse',5);
--- insert into toy(toyId, toyName, belongsToId) values(12,'ball',6);
--- update person set favouritePetId = 5 where personId = 2;
+insert into toy(toyId, toyName, belongsToId) values(11,'mouse',5);
+insert into toy(toyId, toyName, belongsToId) values(12,'ball',6);
+update person set favouritePetId = 5 where personId = 2;
 
-select * from toy;
-select * from pet;
-select * from person;
-
-/* for manual testing of diagrams, commented out to avoid interfering with regression test
-
--- tall, for checking diagram scaling manually
-create table up1 ( upId int PRIMARY KEY);
-create table up2 ( upId int PRIMARY KEY, anotherId int references up1(upId));
-create table up3 ( upId int PRIMARY KEY, anotherId int references up2(upId));
-create table up4 ( upId int PRIMARY KEY, anotherId int references up3(upId));
-create table up5 ( upId int PRIMARY KEY, anotherId int references up4(upId));
-create table up6 ( upId int PRIMARY KEY, anotherId int references up5(upId));
-create table up7 ( upId int PRIMARY KEY, anotherId int references up6(upId));
-create table up8 ( upId int PRIMARY KEY, anotherId int references up7(upId));
-create table up9 ( upId int PRIMARY KEY, anotherId int references up8(upId));
-create table up10 ( upId int PRIMARY KEY, anotherId int references up9(upId));
-create table up11 ( upId int PRIMARY KEY, anotherId int references up10(upId));
-create table up12 ( upId int PRIMARY KEY, anotherId int references up11(upId));
-create table up13 ( upId int PRIMARY KEY, anotherId int references up12(upId));
-create table up14 ( upId int PRIMARY KEY, anotherId int references up13(upId));
-create table up15 ( upId int PRIMARY KEY, anotherId int references up14(upId));
-create table up16 ( upId int PRIMARY KEY, anotherId int references up15(upId));
-create table up17 ( upId int PRIMARY KEY, anotherId int references up16(upId));
-create table up18 ( upId int PRIMARY KEY, anotherId int references up17(upId));
-create table up19 ( upId int PRIMARY KEY, anotherId int references up18(upId));
-create table up20 ( upId int PRIMARY KEY, anotherId int references up19(upId));
-
--- wide, for checking diagram scaling manually
-create table parent ( parentId int PRIMARY KEY, grandParentId int references up20(upId));
-create table child01 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child02 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child03 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child04 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child05 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child06 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child07 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child08 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child09 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child10 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child11 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child12 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child13 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child14 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child15 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child16 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child17 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child18 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child19 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-create table child20 ( childId int PRIMARY KEY, parentId int references parent(parentId));
-*/
