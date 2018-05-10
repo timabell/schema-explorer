b8f66e39dbd8830d591724a29c9441c4d355fff2
Tim Abell <tim@timwise.co.uk>
2018-05-10 18:12:38 +0100

    Quieter postgres setup

--- pg/setup-ssetest.sh
+++ pg/setup-ssetest.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 usr=ssetestusr
 db=ssetest
+echo "removing old test db"
 dropdb $db
 dropuser $usr
 createuser $usr
 createdb $db
-psql -c "alter user $usr with password '$usr'";
-psql -c "alter database $db owner to $usr";
+psql -q -c "alter user $usr with password '$usr'";
+psql -q -c "alter database $db owner to $usr";
 # use psql -e to echo sql along with errors while debugging
-psql -d $db < test-db.sql
-psql -d $db -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $usr;";
+psql -d $db -q < test-db.sql
+psql -d $db -q -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $usr;";
+echo "test postgres db $db and user $usr created"
