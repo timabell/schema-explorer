37985aa1aab0f242226018957ee9f1d38358c568
Tim Abell <tim@timwise.co.uk>
2018-05-10 21:51:38 +0100

    1 second delay

--- run-all.sh
+++ run-all.sh
@@ -2,14 +2,14 @@
 # relative path hack with pwd, otherwise not resolved.
 # create db first with sqlite/setup.sh
 go run sdv.go -driver sqlite -db "`pwd`/sqlite/db/test.db" -port 8081 -live &
-sleep 2
+sleep 1
 ./run-sqlite.sh &
-sleep 2
+sleep 1
 go run sdv.go -driver mssql -db "server=sdv-regression-test.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=sdv-regression-test" -port 8083 -live &
-sleep 2
+sleep 1
 go run sdv.go -driver mssql -db "server=sdv-adventureworks.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=AdventureWorksLT" -port 8084 -live &
-sleep 2
+sleep 1
 go run sdv.go -driver mssql -db "server=sdv-wwi.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=WideWorldImporters" -port 8085 -live &
-sleep 2
+sleep 1
 ./run-pg.sh &
 wait
