3e8cbffa8d975ec39b03f0ea9e53f7c73b5bd317
Tim Abell <tim@timwise.co.uk>
2018-06-04 19:16:32 +0100

    Quieter static logging

--- host/host.go
+++ host/host.go
@@ -64,12 +64,16 @@ func RunServer(driverInfo string, dbConn string, port int, listenOn string, live
 // wrap handler, log requests as they pass through
 func loggingHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dump, err := httputil.DumpRequest(r, false)
-		if err != nil {
-			log.Println("couldn't dump request")
-			panic(err)
+		if strings.HasPrefix(r.URL.Path, "/static") {
+			log.Printf("Request: '%v' | %s %s", r.RemoteAddr, r.Method, r.URL.Path)
+		} else {
+			dump, err := httputil.DumpRequest(r, false)
+			if err != nil {
+				log.Println("couldn't dump request")
+				panic(err)
+			}
+			log.Printf("Request: '%v' | %s", r.RemoteAddr, dump)
 		}
-		log.Printf("Request from '%v'\n%s", r.RemoteAddr, dump)
 		next.ServeHTTP(w, r)
 	})
 }
