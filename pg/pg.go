a5ea45d96cade562a1e50583494db0619a6c9324
Tim Abell <tim@timwise.co.uk>
2018-07-05 18:22:01 +0100

    Unnest pg fk columns

--- pg/pg.go
+++ pg/pg.go
@@ -147,12 +147,13 @@ func readConstraints(dbc *sql.DB, sourceTable *schema.Table, database *schema.Da
 	// todo: parameterise
 	// todo: support multi-column FKs
 	sql := fmt.Sprintf(`select con.contype, col.attname column_name, ftbl.relname, fcol.attname foreign_column, con.conname
-		from pg_constraint con
+		from
+			(select pgc.connamespace, pgc.conrelid, pgc.confrelid, pgc.contype, pgc.conname, unnest(pgc.conkey) as conkey, unnest(pgc.confkey) as confkey from pg_constraint pgc) as con
 			inner join pg_namespace ns on con.connamespace = ns.oid
 			inner join pg_class tbl on tbl.oid = con.conrelid
-			inner join pg_attribute col on col.attrelid = tbl.oid and col.attnum = con.conkey[1]
+			inner join pg_attribute col on col.attrelid = tbl.oid and col.attnum = con.conkey
 			left outer join pg_class ftbl on ftbl.oid = con.confrelid
-			left outer join pg_attribute fcol on fcol.attrelid = ftbl.oid and fcol.attnum = con.confkey[1]
+			left outer join pg_attribute fcol on fcol.attrelid = ftbl.oid and fcol.attnum = con.confkey
 		where ns.nspname = '%s' and tbl.relname = '%s';`,
 		sourceTable.Schema, sourceTable.Name)
 
