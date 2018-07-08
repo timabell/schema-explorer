0facffbf3e70bf5d06023421e1b38dfb1ac90fdf
Tim Abell <tim@timwise.co.uk>
2018-07-08 18:56:30 +0100

    run-all port listing

--- run-all.sh
+++ run-all.sh
@@ -5,6 +5,14 @@
 # see all colours: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux#comment32077818_5947788
 # for (( i = 0; i < 17; i++ )); do echo "$(tput setaf $i)This is ($i) $(tput sgr0)"; done
 
+echo "http://localhost:8081/ - sqlite test"
+echo "http://localhost:8082/ - sqlite chinook"
+echo "http://localhost:8083/ - mssql test"
+echo "http://localhost:8084/ - adventureworks"
+echo "http://localhost:8085/ - wwi (broken)"
+echo "http://localhost:8086/ - pg test"
+echo "Ctrl-C to tear them all down again."
+
 go run sdv.go -name sqlite-test -driver sqlite -db "`pwd`/sqlite/db/test.db" -port 8081 -live 2>&1 | sed "s,.*,$(tput setaf 10)sqlite-test &$(tput sgr0)," &
 
 ./run-sqlite.sh 2>&1 | sed "s,.*,$(tput setaf 11)sqlite &$(tput sgr0)," &
