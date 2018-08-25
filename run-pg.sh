bafc687e4e4273442dacbe270c3e8c627a6ced4d
Tim Abell <tim@timwise.co.uk>
2018-08-25 01:17:58 +0100

    Update run-pg.sh

--- run-pg.sh
+++ run-pg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -
-go run sdv.go -name pg-test -driver pg -db "postgres://ssetestusr:ssetestusr@localhost/ssetest" -port 8086 -live &
+go run sdv.go --display-name pg-test --driver pg --db "postgres://ssetestusr:ssetestusr@localhost/ssetest" --listen-on-port 8086 --live &
 wait
