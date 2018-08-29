3c76d490a950effc1c1f60e4035370bc562855f9
Tim Abell <tim@timwise.co.uk>
2018-08-29 08:40:50 +0100

    Correction to driver choices setup

--- reader/dbReader.go
+++ reader/dbReader.go
@@ -12,7 +12,7 @@ import (
 )
 
 type SdvOptions struct {
-	Driver                *string `short:"d" long:"driver" required:"true" description:"Driver to use" choice:"mssql,pg,sqlite" env:"schemaexplorer_driver"`
+	Driver                *string `short:"d" long:"driver" required:"true" description:"Driver to use" choice:"mssql" choice:"pg" choice:"sqlite" env:"schemaexplorer_driver"`
 	Live                  *bool   `short:"l" long:"live" description:"update html templates & schema information from disk on every page load" env:"schemaexplorer_live"`
 	ConnectionDisplayName *string `short:"n" long:"display-name" description:"A display name for this connection" env:"schemaexplorer_display_name"`
 	ListenOnAddress       *string `short:"a" long:"listen-on-address" description:"address to listen on" default:"localhost" env:"schemaexplorer_listen_on_address"` // localhost so that it's secure by default, only listen for local connections
