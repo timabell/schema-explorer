e9edc8d3106b32ac5d7ad5ecbd9f518bf690fe16
Tim Abell <tim@timwise.co.uk>
2018-10-16 08:28:25 +0100

    Document gox attempt

--- build-mac.sh
+++ build-mac.sh
@@ -4,3 +4,8 @@ echo running mac build
 env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o bin/mac/schemaexplorer
 cp -r templates bin/mac/
 cp -r static bin/mac/
+
+# also tried gox https://github.com/mitchellh/gox
+# gox -cgo -output="bin/{{.Dir}}_{{.OS}}_{{.Arch}}" -os="darwin" -arch="amd64"
+# but has same issues with the sqlite C code
+
