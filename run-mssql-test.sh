84019e1437df16e506c672c6ad9e7c82ae6d6cf2
Tim Abell <tim@timwise.co.uk>
2018-10-22 21:44:55 +0100

    Update run-mssql-test to use local docker

--- run-mssql-test.sh
+++ run-mssql-test.sh
@@ -4,6 +4,6 @@ export schemaexplorer_driver=mssql
 export schemaexplorer_display_name=mssql-test
 export schemaexplorer_live=true
 export schemaexplorer_listen_on_port=8084
-export schemaexplorer_mssql_connection_string="server=sdv-regression-test.database.windows.net;user id=sdvRO;password=Startups 4 the rest of us;database=sdv-regression-test"
+export schemaexplorer_mssql_connection_string="server=localhost;user id=sa;password=GithubIs2broken;database=sdv-regression-test"
 go run sdv.go 2>&1 | sed "s,.*,$(tput setaf 13)mssql-test &$(tput sgr0)," &
 wait
