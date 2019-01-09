eb7ea50e44f06b5dd7d32d8197706fa8731a56e5
Tim Abell <tim@timwise.co.uk>
2019-01-09 00:08:33 +0000

    Version bump v0.45

--- about/about.go
+++ about/about.go
@@ -8,7 +8,7 @@ type AboutType struct {
 
 var About = AboutType{
 	ProductName: "Sql Schema Explorer",
-	Version:     "0.44",
+	Version:     "0.45",
 	Website:     "http://schemaexplorer.io/",
 	Email:       "sse@timwise.co.uk",
 }
