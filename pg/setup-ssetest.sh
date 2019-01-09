736c4c6b9ba414400a59c08833b2beaf249b9b88
Tim Abell <tim@timwise.co.uk>
2019-01-09 00:07:04 +0000

    Give schema privs in pg for new schema

--- pg/setup-ssetest.sh
+++ pg/setup-ssetest.sh
@@ -11,5 +11,6 @@ psql -q -c "alter database $db owner to $usr";
 # use psql -e to echo sql along with errors while debugging
 psql -d $db -q < test-db.sql
 psql -d $db -q -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $usr;";
+psql -d $db -q -c "GRANT ALL PRIVILEGES ON SCHEMA \"identity\" TO $usr;";
 psql -d $db -q -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA \"identity\" TO $usr;";
 echo "test postgres db $db and user $usr created"
