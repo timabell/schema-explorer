686355e7e7c11186fb512acad5a3b01b31c34d35
Tim Abell <tim@timwise.co.uk>
2019-01-09 08:36:24 +0000

    Quieter sqlite test setup

--- sqlite/setup.sh
+++ sqlite/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -d db ]; then
-  echo 'removing old test db'
+  # echo 'removing old test db'
   rm -rf db
 fi
 mkdir -p db
 sqlite3 db/test.db < test-db.sql
-echo 'test sqlite db created'
+# echo 'test sqlite db created'
