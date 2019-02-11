c834f0c279dc92ddd1a61427ec1f9c8a61363c86
Tim Abell <tim@timwise.co.uk>
2019-02-11 18:01:22 +0000

    wip - broken http get of root test

--- sse_http_test.go
+++ sse_http_test.go
@@ -1,5 +1,17 @@
 package main
 
-import(
+import (
+	"bitbucket.org/timabell/sql-data-viewer/host"
 	"net/http"
+	"net/http/httptest"
+	"testing"
 )
+
+func Test_Http(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	host.Router().ServeHTTP(response, request)
+	if response.Code != 200 {
+		t.Fatalf("%d status for /", response.Code)
+	}
+}
