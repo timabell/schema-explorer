c11d6d2742c9477cf933184e8e93f50a9df3ce78
Tim Abell <tim@timwise.co.uk>
2019-02-12 18:03:35 +0000

    Reduce scope of copyright vars

--- licensing/licensing.go
+++ licensing/licensing.go
@@ -8,19 +8,19 @@ import (
 )
 
 // at least 6 months from when this is released into the wild
-var Expiry = time.Date(2019, time.April, 2, 0, 0, 0, 0, time.UTC)
-var CopyrightYear = 2019
+var expiry = time.Date(2019, time.April, 2, 0, 0, 0, 0, time.UTC)
+var copyrightYear = 2019
 
 func Licensing() {
-	if time.Now().After(Expiry) {
+	if time.Now().After(expiry) {
 		log.Panicf("Expired trial, contact %s to obtain a license", about.About.Email)
 	}
 }
 
 func LicenseText() string {
-	return fmt.Sprintf("This pre-release software will expire on: %s, contact %s for a license.", Expiry, about.About.Email)
+	return fmt.Sprintf("This pre-release software will expire on: %s, contact %s for a license.", expiry, about.About.Email)
 }
 
 func CopyrightText() string {
-	return fmt.Sprintf("Copyright 2015-%d Tim Abell", CopyrightYear)
+	return fmt.Sprintf("Copyright 2015-%d Tim Abell", copyrightYear)
 }
