026a098dbacc8b11121b4f4a57177cd397384628
Tim Abell <tim@timwise.co.uk>
2019-03-07 20:58:09 +0000

    Fix mssql docker setup
    Can't put a comment there it seems

--- mssql/docker-first-run.sh
+++ mssql/docker-first-run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -v
 
 # https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-2017
+
+# map current folder in so that we can run sql files with sqlcmd
+
 docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=GithubIs2broken' \
    -p 1433:1433 --name mssql1 \
-   -v `pwd`:/src \ # so that we can run sql files with sqlcmd
+   -v `pwd`:/src \
    -d mcr.microsoft.com/mssql/server:2017-latest
