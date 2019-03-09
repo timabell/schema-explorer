43cacbad8aa27ea9bdeca11d933f4621c1dde532
Tim Abell <tim@timwise.co.uk>
2019-03-09 21:50:09 +0000

    Kill access to setup once configured

--- http/setup-handlers.go
+++ http/setup-handlers.go
@@ -1,24 +1,44 @@
 package http
 
 import (
+	"bitbucket.org/timabell/sql-data-viewer/options"
 	"bitbucket.org/timabell/sql-data-viewer/render"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func SetupHandler(resp http.ResponseWriter, req *http.Request) {
+	if RedirectIfConfigured(resp, req) {
+		return
+	}
 	layoutData := requestSetup()
 	render.ShowSelectDriver(resp, layoutData)
 }
 
 func SetupDriverHandler(resp http.ResponseWriter, req *http.Request) {
+	if RedirectIfConfigured(resp, req) {
+		return
+	}
 	layoutData := requestSetup()
 	driverName := mux.Vars(req)["driver"]
 	render.ShowSetupDriver(resp, layoutData, driverName)
 }
 
 func SetupDriverPostHandler(resp http.ResponseWriter, req *http.Request) {
+	if RedirectIfConfigured(resp, req) {
+		return
+	}
 	layoutData := requestSetup()
 	driverName := mux.Vars(req)["driver"]
 	render.RunSetupDriver(resp, req, layoutData, driverName)
+}
+
+func RedirectIfConfigured(resp http.ResponseWriter, req *http.Request) (isConfigured bool) {
+	// Security: Don't allow use of setup if already configured.
+	// This allows local users to easily configure on startup, but prevents admin-configured copies from being modified by wayward web users.
+	if options.Options.Driver != nil {
+		http.Redirect(resp, req, "/", http.StatusFound)
+		return true
+	}
+	return false
 }
