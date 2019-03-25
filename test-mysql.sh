37a01d792a67d6ae2893a46aa2612f6ca73ee8c7
Tim Abell <tim@timwise.co.uk>
2019-03-25 08:47:41 +0000

    Remove old comment in test-mysql.sh

--- test-mysql.sh
+++ test-mysql.sh
@@ -6,7 +6,6 @@ echo "=================="
 
 (cd mysql/ && ./setup.sh)
 
-# relative path hack with pwd, otherwise not resolved.
 export schemaexplorer_driver=mysql
 export schemaexplorer_live=false
 export schemaexplorer_mysql_database=ssetest
