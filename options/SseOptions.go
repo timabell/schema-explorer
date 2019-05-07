1a56d81d40bf9aae5c41395d3a25dff8416bd037
Tim Abell <tim@timwise.co.uk>
2019-05-07 08:38:34 +0100

    Move arg parsing into options package
    Results in import loop. Hmm

--- options/SseOptions.go
+++ options/SseOptions.go
@@ -1,5 +1,14 @@
 package options
 
+import (
+	"bitbucket.org/timabell/sql-data-viewer/reader"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func init() {
 }
 
@@ -15,3 +24,114 @@ type SseOptions struct {
 var Options = &SseOptions{}
 
 //var ArgParser = flags.NewParser(Options, flags.Default)
+
+func SetupArgs() {
+	//_, err := options.ArgParser.ParseArgs(os.Args)
+	//if err != nil {
+	//	// only write out help if not already being written
+	//	if e, ok := err.(*flags.Error); !ok || e.Type != flags.ErrHelp {
+	//		options.ArgParser.WriteHelp(os.Stdout)
+	//	}
+	//	os.Stdout.WriteString("\n")
+	//	os.Stdout.WriteString("Environment Variables:\n")
+	//	os.Stdout.WriteString("  Environment variables can be used instead of of the command line arguments.\n")
+	//	os.Stdout.WriteString("  The environment variable names can be found at the end of each option's\n")
+	//	os.Stdout.WriteString("  description above in the form [$env_var_name].\n")
+	//	os.Stdout.WriteString("  These can then be set with: env_var_name=value\n")
+	//	os.Stdout.WriteString("  <3 <3 <3 Because we love https://12factor.net/config <3 <3 <3\n")
+	//	os.Stdout.WriteString("\n")
+	//	os.Exit(1)
+	//}
+
+	if os.Getenv("schemaexplorer_driver") != "" {
+		envDriver := os.Getenv("schemaexplorer_driver")
+		Options.Driver = &envDriver
+	}
+	if os.Getenv("schemaexplorer_listen_on_port") != "" {
+		envPort := os.Getenv("schemaexplorer_listen_on_port")
+		portInt64, err := strconv.ParseInt(envPort, 0, 0)
+		if err != nil {
+			panic(err)
+		}
+		portInt := int(portInt64)
+		Options.ListenOnPort = &portInt
+	}
+	if os.Getenv("schemaexplorer_live") != "" {
+		envLive := os.Getenv("schemaexplorer_live")
+		boolLive, err := strconv.ParseBool(envLive)
+		if err != nil {
+			panic(err)
+		}
+		Options.Live = &boolLive
+	}
+	if os.Getenv("schemaexplorer_display_name") != "" {
+		envName := os.Getenv("schemaexplorer_display_name")
+		Options.ConnectionDisplayName = &envName
+	}
+	if os.Getenv("schemaexplorer_peek_config_path") != "" {
+		envPeek := os.Getenv("schemaexplorer_Peek")
+		Options.PeekConfigPath = &envPeek
+	}
+
+	var driverStrings []string
+	drivers := reader.Drivers
+	for _, driver := range drivers {
+		driverStrings = append(driverStrings, driver.Name)
+	}
+	for _, driver := range drivers {
+		for key, driverOpt := range driver.NewOptions {
+			flag.StringVar(driverOpt.Value, fmt.Sprintf("%s-%s", driver.Name, key), "", driverOpt.Description)
+		}
+	}
+
+	supportedDrivers := strings.Join(driverStrings, ", ")
+	driver = flag.String("driver", "", "Driver to use. Available drivers: "+supportedDrivers)
+	port = flag.Int("listen-on-port", 0, "Port to listen on. Defaults to random unused high-number.")
+	address = flag.String("listen-on-address", "", "Address to listen on. Set to 0.0.0.0 to allow access to schema-explorer from other computers. Listens on localhost by default only allow connections from this machine.")
+	live = flag.Bool("live", false, "Update html templates & schema information on from every page load.")
+	name = flag.String("display-name", "", "A display name for this connection.")
+	peekPath = flag.String("peek-config-path", "", "Path to peek configuration file. Defaults to the file included with schema explorer.")
+}
+
+var driver *string
+var port *int
+var address *string
+var live *bool
+var name *string
+var peekPath *string
+
+func ReadArgs() {
+	flag.Parse()
+
+	for _, driver := range reader.Drivers {
+		for key, driverOpt := range driver.NewOptions {
+			if *driverOpt.Value != "" {
+				continue // command line flags take precedence over environment
+			}
+			envKey := fmt.Sprintf("schemaexplorer_%s_%s", driver.Name, strings.Replace(key, "-", "_", 0))
+			if os.Getenv(envKey) != "" {
+				envValue := os.Getenv(envKey)
+				*driverOpt.Value = envValue
+			}
+		}
+	}
+
+	if Options.Driver == nil && *driver != "" {
+		Options.Driver = driver
+	}
+	if Options.ListenOnPort == nil {
+		Options.ListenOnPort = port
+	}
+	if Options.ListenOnAddress == nil && *address != "" {
+		Options.ListenOnAddress = address
+	}
+	if Options.Live == nil {
+		Options.Live = live
+	}
+	if Options.ConnectionDisplayName == nil {
+		Options.ConnectionDisplayName = name
+	}
+	if Options.PeekConfigPath == nil {
+		Options.PeekConfigPath = peekPath
+	}
+}
--- sse.go
+++ sse.go
@@ -18,118 +18,16 @@ import (
 	_ "bitbucket.org/timabell/sql-data-viewer/mysql"
 	"bitbucket.org/timabell/sql-data-viewer/options"
 	_ "bitbucket.org/timabell/sql-data-viewer/pg"
-	"bitbucket.org/timabell/sql-data-viewer/reader"
 	"bitbucket.org/timabell/sql-data-viewer/serve"
 	_ "bitbucket.org/timabell/sql-data-viewer/sqlite"
-	"flag"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
 	licensing.EnforceLicensing()
 
-	//_, err := options.ArgParser.ParseArgs(os.Args)
-	//if err != nil {
-	//	// only write out help if not already being written
-	//	if e, ok := err.(*flags.Error); !ok || e.Type != flags.ErrHelp {
-	//		options.ArgParser.WriteHelp(os.Stdout)
-	//	}
-	//	os.Stdout.WriteString("\n")
-	//	os.Stdout.WriteString("Environment Variables:\n")
-	//	os.Stdout.WriteString("  Environment variables can be used instead of of the command line arguments.\n")
-	//	os.Stdout.WriteString("  The environment variable names can be found at the end of each option's\n")
-	//	os.Stdout.WriteString("  description above in the form [$env_var_name].\n")
-	//	os.Stdout.WriteString("  These can then be set with: env_var_name=value\n")
-	//	os.Stdout.WriteString("  <3 <3 <3 Because we love https://12factor.net/config <3 <3 <3\n")
-	//	os.Stdout.WriteString("\n")
-	//	os.Exit(1)
-	//}
-
-	if os.Getenv("schemaexplorer_driver") != "" {
-		envDriver := os.Getenv("schemaexplorer_driver")
-		options.Options.Driver = &envDriver
-	}
-	if os.Getenv("schemaexplorer_listen_on_port") != "" {
-		envPort := os.Getenv("schemaexplorer_listen_on_port")
-		portInt64, err := strconv.ParseInt(envPort, 0, 0)
-		if err != nil {
-			panic(err)
-		}
-		portInt := int(portInt64)
-		options.Options.ListenOnPort = &portInt
-	}
-	if os.Getenv("schemaexplorer_live") != "" {
-		envLive := os.Getenv("schemaexplorer_live")
-		boolLive, err := strconv.ParseBool(envLive)
-		if err != nil {
-			panic(err)
-		}
-		options.Options.Live = &boolLive
-	}
-	if os.Getenv("schemaexplorer_display_name") != "" {
-		envName := os.Getenv("schemaexplorer_display_name")
-		options.Options.ConnectionDisplayName = &envName
-	}
-	if os.Getenv("schemaexplorer_peek_config_path") != "" {
-		envPeek := os.Getenv("schemaexplorer_Peek")
-		options.Options.PeekConfigPath = &envPeek
-	}
-
-	var driverStrings []string
-	for _, driver := range reader.Drivers {
-		driverStrings = append(driverStrings, driver.Name)
-	}
-	for _, driver := range reader.Drivers {
-		for key, driverOpt := range driver.NewOptions {
-			flag.StringVar(driverOpt.Value, fmt.Sprintf("%s-%s", driver.Name, key), "", driverOpt.Description)
-		}
-	}
-
-	supportedDrivers := strings.Join(driverStrings, ", ")
-	driver := flag.String("driver", "", "Driver to use. Available drivers: "+supportedDrivers)
-	port := flag.Int("listen-on-port", 0, "Port to listen on. Defaults to random unused high-number.")
-	address := flag.String("listen-on-address", "", "Address to listen on. Set to 0.0.0.0 to allow access to schema-explorer from other computers. Listens on localhost by default only allow connections from this machine.")
-	live := flag.Bool("live", false, "Update html templates & schema information on from every page load.")
-	name := flag.String("display-name", "", "A display name for this connection.")
-	peekPath := flag.String("peek-config-path", "", "Path to peek configuration file. Defaults to the file included with schema explorer.")
-
-	flag.Parse()
-
-	for _, driver := range reader.Drivers {
-		for key, driverOpt := range driver.NewOptions {
-			if *driverOpt.Value != ""{
-				continue // command line flags take precedence over environment
-			}
-			envKey := fmt.Sprintf("schemaexplorer_%s_%s", driver.Name, strings.Replace(key, "-", "_", 0))
-			if os.Getenv(envKey) != "" {
-				envValue := os.Getenv(envKey)
-				*driverOpt.Value = envValue
-			}
-		}
-	}
-
-	if options.Options.Driver == nil && *driver != "" {
-		options.Options.Driver = driver
-	}
-	if options.Options.ListenOnPort == nil {
-		options.Options.ListenOnPort = port
-	}
-	if options.Options.ListenOnAddress == nil && *address != "" {
-		options.Options.ListenOnAddress = address
-	}
-	if options.Options.Live == nil {
-		options.Options.Live = live
-	}
-	if options.Options.ConnectionDisplayName == nil {
-		options.Options.ConnectionDisplayName = name
-	}
-	if options.Options.PeekConfigPath == nil {
-		options.Options.PeekConfigPath = peekPath
-	}
+	options.SetupArgs()
+	options.ReadArgs()
 
 	log.Printf("%s\n  %s\n  %s\n  Feeback/support/contact: <%s>",
 		about.About.Summary(),
