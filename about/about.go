920d3daf143d280ae205071fa28685739a9cbd6f
Tim Abell <tim@timwise.co.uk>
2019-11-09 17:37:35 +0000

    version bump .67

--- about/about.go
+++ about/about.go
@@ -10,7 +10,7 @@ var gitVersion = "local-dev-build"
 
 var About = AboutType{
 	ProductName: "Sql Schema Explorer",
-	Version:     "0.66-" + gitVersion,
+	Version:     "0.67-" + gitVersion,
 	Website:     "http://schemaexplorer.io/",
 	Email:       "tim@schemaexplorer.io",
 }
