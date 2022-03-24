2ecbd597222552f8404d42876198a72e41ca7a60
Tim Abell <tim@timwise.co.uk>
2022-03-24 21:43:45 +0000

    Update pg connections in test sh for docker

--- pg/run-pg-multi-db.sh
+++ pg/run-pg-multi-db.sh
@@ -4,6 +4,9 @@ cd ..
 export schemaexplorer_driver=pg
 export schemaexplorer_live=true
 export schemaexplorer_listen_on_port=8812
-export schemaexplorer_pg_host=/var/run/postgresql/
+export schemaexplorer_pg_host=localhost
+export schemaexplorer_pg_user=postgres
+export schemaexplorer_pg_password=postgres
+export schemaexplorer_pg_ssl_mode=disable
 go run sse.go 2>&1 | sed "s,.*,$(tput setaf 14)pg-test &$(tput sgr0)," &
 wait
--- pg/run-pg.sh
+++ pg/run-pg.sh
@@ -5,6 +5,6 @@ export schemaexplorer_driver=pg
 export schemaexplorer_display_name=pg-test
 export schemaexplorer_live=true
 export schemaexplorer_listen_on_port=8811
-export schemaexplorer_pg_connection_string="postgres://ssetestusr:ssetestusr@localhost/ssetest"
+export schemaexplorer_pg_connection_string="postgres://ssetestusr:ssetestusr@localhost/ssetest?sslmode=disable"
 go run sse.go 2>&1 | sed "s,.*,$(tput setaf 14)pg-test &$(tput sgr0)," &
 wait
