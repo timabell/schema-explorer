dd0b203db8a368681ef014749397c3595a0aee3d
Tim Abell <tim@timwise.co.uk>
2024-01-28 01:51:18 +0000

    ci: Test mysql
    Switch to java test parser -
    https://github.com/dorny/test-reporter/issues/241#issuecomment-1536260009
    
    Currently the connection test passes and then a very odd failure on
    reading columns which I don't get locally.

---
+++ mysql/run-mysql-connectionstring.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+cd ..
+
+export schemaexplorer_driver=mysql
+export schemaexplorer_display_name=mysql-test
+export schemaexplorer_live=false
+export schemaexplorer_listen_on_port=8831
+export schemaexplorer_mysql_connection_string="ssetestusr:ssetestusrpass@tcp(localhost:3306)/ssetest"
+go run sse.go
